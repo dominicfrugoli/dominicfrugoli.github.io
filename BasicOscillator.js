d62dab66e8bf04bd5ada114257586a0ed102ecfd
dominicfrugoli <dominicfrugoli@gmail.com>
2023-04-19 14:50:27 -0500

    added sine function

--- BasicOscillator.js
+++ BasicOscillator.js
@@ -6,7 +6,10 @@ const amp = new GainNode(context);
 osc.connect(amp);
 amp.connect(context.destination);
 
-amp.gain.value = 0.25;
-osc.frequency(300);
-osc.start(0.0);
-osc.stop(2.0);
+function Sine()
+{
+    amp.gain.value = 0.25;
+    osc.frequency(300);
+    osc.start(0.0);
+    osc.stop(2.0);
+}
