38de950e231d75353f195e0f084c1b962eb78ebd
dominicfrugoli <dominicfrugoli@gmail.com>
2023-04-19 14:40:00 -0500

    Added oscillator

---
+++ BasicOscillator.js
@@ -0,0 +1,11 @@
+const context = new AudioContext();
+
+const osc = new OscillatorNode(context);
+const amp = new GainNode(context);
+
+osc.connect(amp);
+amp.connect(context.destination);
+
+amp.gain.value = 0.25;
+osc.start(0.0);
+osc.stop(2.0);
