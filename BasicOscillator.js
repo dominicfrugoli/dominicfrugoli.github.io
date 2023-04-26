e62d80a7e905ec4e5e9b2735c3070c4f1f060b53
dominicfrugoli <dominicfrugoli@gmail.com>
2023-04-26 13:38:01 -0500

    trying sound functions

--- BasicOscillator.js
+++ BasicOscillator.js
@@ -1,15 +1,49 @@
-const context = new AudioContext();
 
-const osc = new OscillatorNode(context);
-const amp = new GainNode(context);
 
-osc.connect(amp);
-amp.connect(context.destination);
+var osc, freq, amp, isPlaying;
 
-function Sine()
+var columnWidth = 50;
+var lastColumn = 1;
+
+
+function setup() 
 {
-    amp.gain.value = 0.25;
-    osc.frequency(300);
-    osc.start(0.0);
-    osc.stop(2.0);
+  let canvas = createCanvas(640, 360);
+  canvas.mouseIsPressed(playOsc);
+  osc = new p5.Oscillator('sine');
+  colorMode(HSB, height, height, height)
+  noStroke();
+  background(255);
+}
+
+
+function draw()
+{                        
+    let whichColumn = mouseX/columnWidth;
+    if(whichColumn != lastColumn)
+    {
+      var columnX = whichColumn * columnWidth;
+      fill(mouseY, height, height);
+      rect(columnX, 0, columnWidth, height);
+      lastColumn = whichColumn;
+    }
+    if(isPlaying)
+    {
+        osc.freq(freq, 0.1);
+        osc.amp(amp, 0.1);
+    }
+  
+}
+
+
+function playOsc()
+{
+    osc.start();
+    isPlaying = true;
+}
+
+function mouseReleased()
+{
+    osc.amp(0, 0.5);
+    isPlaying = false;
 }
